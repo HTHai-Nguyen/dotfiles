278c9500c8b7665833a65310927625ed994adb9e
HTHai-Nguyen <21520808@ms.uit.edu.vn>
2025-11-10 19:42:01 +0700

    Add fzf plugin to lazyvim

---
+++ neovim/lazyvim/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,6 @@
+return {
+  -- Disable Telescope and its extensions
+  { "nvim-telescope/telescope.nvim", enabled = false },
+  { "nvim-telescope/telescope-fzf-native.nvim", enabled = false },
+  { "nvim-telescope/telescope-ui-select.nvim", enabled = false },
+}
