8ea7d240cc6ce79afc19bf72b20388918e49952a
HTHai-Nguyen <21520808@ms.uit.edu.vn>
2025-12-04 11:14:57 +0700

    add feature auto_symlink

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -xe
+
+set -euo pipefail
 
 # GNU Stow format XDG except .zshrc & .bashrc
 
@@ -13,31 +14,28 @@ HOME_DIR="$HOME"
 MODULES_DIR="$DOTFILES_DIR/scripts/modules"
 
 # Detect package manager
-PKG_MANGER=""
-for m in "$MODULES_DIR"/*.sh; do
-  if [ -f "$m" ]; then
-    base_name=$(basename "$m" .sh)
-    # file_name="${m##*/}"  # If basename command not found
-    # base_name="${file_name%.sh}"
-    if command -v "$base_name" >/dev/null 2>&1; then
-      PKG_MANAGER="$base_name"
-      source "$m"
-      break
-    fi
-  fi
-done
-
-if [ -z "$PKG_MANAGER" ]; then
-  echo "No supported packages manager!"
-  exit 1
-fi
-
-echo "Module $(basename "$PKG_MANAGER") detected"
+# PKG_MANGER=""
+# for m in "$MODULES_DIR"/*.sh; do
+#   if [ -f "$m" ]; then
+#     base_name=$(basename "$m" .sh)
+#     # file_name="${m##*/}"  # If basename command not found
+#     # base_name="${file_name%.sh}"
+#     if command -v "$base_name" >/dev/null 2>&1; then
+#       PKG_MANAGER="$base_name"
+#       source "$m"
+#       break
+#     fi
+#   fi
+# done
+#
+# if [ -z "$PKG_MANAGER" ]; then
+#   echo "No supported packages manager!"
+#   exit 1
+# fi
 
 # package installed check
 installed() {
-  local bin="$1"
-  command -v "$bin" >/dev/null 2>&1
+  command -v "$1" >/dev/null 2>&1
   # local pkg="$1"
   # if pkg_installed "$pkg"; then
   #   return 0
@@ -51,97 +49,73 @@ symlink() {
   local target="$2"
   if [ -d "$DOTFILES_DIR/$folder" ]; then
     stow -D "$folder" -d "$DOTFILES_DIR" -t "$target" >/dev/null 2>&1 || true # Unstow to avoid conflict
-    stow -V "$folder" -d "$DOTFILES_DIR" -t "$target"
-    echo "Stowed $folder to $target"
+    stow "$folder" -d "$DOTFILES_DIR" -t "$target"
+    echo "Complete stow $folder -> $target"
     echo
   else
     echo "Folder $DOTFILES_DIR/$folder nout found. Skipping. "
   fi
 }
 
+auto_symlink() {
+  local module="$1"
+  local target="$2"
+  local subdirs=$(find "$DOTFILES_DIR/$module" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
+  for sub in $subdirs; do
+    if installed "$sub"; then
+      symlink "$module/$sub" "$target"
+    fi
+  done
+}
+
 echo "Symboliclink configs using Stow"
 cd "$DOTFILES_DIR"
 
-# keyboards: only kanata
-echo "Stow keyboards"
+# shells: stow all into $HOME
+echo "=====Stow shells====="
+symlink "shells" "$HOME_DIR"
+echo
+
+# keyboards: only kanata (fixed, not auto)
+echo "=====Stow keyboards====="
 symlink "keyboards/kanata" "$CONFIG_DIR"
 echo
 
-# multiplxers
-echo "Stow multiplxers"
-for mul in multiplxers/*; do
-  pkg="$(basename "$mul")"
-  if installed "$pkg"; then
-    symlink "multiplxers/$pkg" "$CONFIG_DIR"
-  else
-    echo " - $pkg not installed. Skipping..."
-  fi
-done
+# multiplexers
+echo "=====Stow multiplexers====="
+auto_symlink "multiplexers" "$CONFIG_DIR"
 echo
 
 # neovim: select a distribution & uncomment it
+echo "=====Stow neovim/?====="
 # symlink "neomvim/lazyvim" "$CONFIG_DIR"
 # symlink "neomvim/nvchad" "$CONFIG_DIR"
 # symlink "neomvim/ecovim" "$CONFIG_DIR"
 # symlink "neomvim/kickstart" "$CONFIG_DIR"
-echo " - Stow neovim/?"
 echo
 
 # scripts: ignore
-echo "scripts ignore"
-echo
-
-# shells: stow all into $HOME
-echo "Stow shells"
-symlink "shells" "$HOME_DIR"
+echo "=====Scripts ignore====="
 echo
 
 # statusbars
-echo "Stow statusbars"
-for bar in statusbars/*; do
-  pkg="$(basename "$bar")"
-  if installed "$pkg"; then
-    symlink "statusbars/$pkg" "$CONFIG_DIR"
-  else
-    echo " - $pkg not installed. Skipping..."
-  fi
-done
+echo "=====Stow statusbars======"
+auto_symlink "statusbars" "$CONFIG_DIR"
 echo
 
 # terminals
 echo "Stow terminals"
-for term in terminals/*; do
-  pkg="$(basename "$term")"
-  if installed "$pkg"; then
-    symlink "terminals/$pkg" "$CONFIG_DIR"
-  else
-    echo " - $pkg not installed. Skipping..."
-  fi
-done
+auto_symlink "terminals" "$CONFIG_DIR"
 echo
 
 # tools
 echo "Stow tools"
-for tool in tools/*; do
-  pkg="$(basename "$tool")"
-  if installed "$pkg"; then
-    symlink "tools/$pkg" "$CONFIG_DIR"
-  else
-    echo " - $pkg not installed. Skipping..."
-  fi
-done
+auto_symlink "tools" "$CONFIG_DIR"
 echo
 
 # Window manager
 echo "Stow window manager"
-for wm in WMs/*; do
-  pkg="$(basename "$wm")"
-  if installed "$pkg"; then
-    symlink "Wms/$pkg" "$CONFIG_DIR"
-  else
-    echo " - $pkg not installed. Skipping..."
-  fi
-done
+auto_symlink "WMs" "$CONFIG_DIR"
 
 echo
 echo "All done! Dotilfes successfully GNU Stow."
