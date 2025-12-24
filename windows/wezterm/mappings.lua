2d3883a483b54faf0838c8eec1400980adc64444
HTHai-Nguyen <21520808@ms.uit.edu.vn>
2025-12-24 23:10:12 +0700

    config nushell & wezterm multiplexer for windows

---
+++ windows/wezterm/mappings.lua
@@ -0,0 +1,137 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local M = wezterm.config_builder()
+
+--- Leader key: CTRL+a 
+M.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+M.keys = {
+  -- Navigations (vim-like)
+  { key = 'h', mods = 'CTRL', action = act.ActivatePaneDirection('Left') },
+  { key = 'j', mods = 'CTRL', action = act.ActivatePaneDirection('Down') },
+  { key = 'k', mods = 'CTRL', action = act.ActivatePaneDirection('Up') },
+  { key = 'l', mods = 'CTRL', action = act.ActivatePaneDirection('Right') },
+
+  --- Workspaces / Sessions
+  { key = 's', mods = 'LEADER', action = act.ShowLauncherArgs { flags = 'WORKSPACES' }, },
+
+  -- Create new Workspace
+  { key = 'N', mods = 'LEADER|SHIFT', action = act.SwitchToWorkspace },
+
+  -- Close Workspace
+  {
+    key = 'X', mods = 'LEADER|SHIFT',
+    action = wezterm.action_callback(function(window, pane)
+      local workspace = window:active_workspace()
+      window:perform_action(act.InputSelector {
+        title = "Do you want to Kill Workspace: " .. workspace .. "?",
+        choices = {
+          { label = "Yes", id = "yes" },
+          { label = "No", id = "no" },
+        },
+        action = wezterm.action_callback(function(inner_window, inner_pane, id, label)
+          if id == "yes" then
+            for _, win in ipairs(wezterm.mux.all_windows()) do
+              if win:get_workspace() == workspace then
+                win:gui_window():perform_action(act.CloseCurrentTab{ confirm = false }, inner_pane)
+              end
+            end
+          end
+        end),
+      }, pane)
+    end),
+  },
+
+  -- Move in Workspace
+  { key = '(', mods = 'LEADER|SHIFT', action = act.SwitchWorkspaceRelative(1), },
+  { key = ')', mods = 'LEADER|SHIFT', action = act.SwitchWorkspaceRelative(-1), },
+
+  -- Rename Workspace
+  {
+    key = '$', mods = 'LEADER|SHIFT', 
+    action = act.PromptInputLine { 
+      description = "Rename Workspace:",
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+          end
+        end),
+    },
+  },
+
+  --- Tabs / Windows ---
+  -- Create new tab
+  { key = 'c', mods = 'LEADER', action = act.SpawnTab('CurrentPaneDomain'), },
+
+  -- Switch tabs
+  { key = 'n', mods = 'LEADER', action = act.ActivateTabRelative(1) },
+  { key = 'p', mods = 'LEADER', action = act.ActivateTabRelative(-1) },
+  
+  -- Rename tab
+  {
+      key = ',', mods = 'LEADER',
+      action = act.PromptInputLine {
+        description = 'Rename current Tab:',
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      },
+  },
+
+  -- List tabs
+  { key = 'w', mods = 'LEADER', action = act.ShowLauncherArgs { flags = 'TABS' }, },
+
+  -- Close tab
+  { key = '&', mods = 'LEADER|SHIFT', action = act.CloseCurrentTab { confirm = false }, },
+
+
+  --- Panes ---
+  -- Split pane: '%' for Vertical and '"' for Horizontal
+  { key = '%', mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+  { key = '"', mods = 'LEADER|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
+
+  -- Convert pane into a tab
+  { 
+    key = '!', mods = 'LEADER|SHIFT',
+    action = wezterm.action_callback(function(win, pane)
+      local tab, window = pane:move_to_new_tab()
+    end),
+  },
+
+  -- Close pane
+  { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true }, },
+  
+  -- Zoom pane
+  { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState, },
+
+  -- Resize
+  {
+    key = 'r', mods = 'LEADER',
+    action = act.ActivateKeyTable {
+      name = 'resize_pane',
+      one_shot = false,
+    },
+  },  -- Reload config
+  {
+    key = 'R', mods = 'LEADER|SHIFT', action = act.ReloadConfiguration,
+  },
+
+}
+
+
+-- Key tables for resize
+M.key_tables = {
+  resize_pane = {
+    { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
+    { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
+    { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
+    { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
+    -- Press Enter / Escape to exit resize mode
+    { key = 'Escape', action = 'PopKeyTable' },
+  },
+}
+
+return M
--- windows/wezterm/wezterm.lua
+++ windows/wezterm/wezterm.lua
@@ -1,9 +1,19 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+local mappings = require 'mappings'
 local config = wezterm.config_builder()
+local act = wezterm.action
+
+--- Keymap ---
+config.leader = mappings.leader
+config.keys = mappings.keys
+config.key_tables = mappings.key_tables
+
+config.max_fps = 120
 
 --- Shell profiles ---
 -- Set default shell
 config.default_prog = { "nu" }
+config.default_cwd = "D:/Codespace"
 config.launch_menu = {
   {
     label = "Nushell",
@@ -37,5 +47,46 @@ config.window_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "TITLE | RESIZE"
 config.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
+
+--- Status bar ---
+-- config.use_fancy_tab_bar = true
+-- config.status_update_interval = 1000
+
+wezterm.on('update-right-status', function(window, pane)
+  local name = window:active_key_table()
+  if name then
+    name = 'TABLE: ' .. name
+  end
+  window:set_right_status(name or '')
+end)
+
+--- Resurection
+config.unix_domains = {
+  {
+    name = 'unix',
+  },
+}
+config.default_gui_startup_args = { 'connect', 'unix' }
+
+-- config.keys = {
+--   {
+--     key = 'r', mods = 'CTRL|ALT',
+--     action = act.ActivateKeyTable {
+--       name = 'resize_pane',
+--       one_slotl= false,
+--     },
+--   },
+-- }
+--
+-- config.key_tables = {
+--   resize_pane = {
+--     { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
+--     { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
+--     { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
+--     { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
+--     -- Press Enter / Escape to exit resize mode
+--     { key = 'Escape', action = 'PopKeyTable' },
+--   },
+-- }
 
 return config
