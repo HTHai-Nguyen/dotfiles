5b78405655203eb81d124db61a12507db82dc1de
HTHai-Nguyen <21520808@ms.uit.edu.vn>
2026-03-25 23:52:15 +0700

    edit on fedora

--- terminals/wezterm/wezterm.lua
+++ terminals/wezterm/wezterm.lua
@@ -1,28 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-<<<<<<< HEAD
-return {
-
-	-- Appearance
-	font_size = 13.0,
-	window_background_opacity = 0.8,
-
-	-- mouse keybindings
-	--
-	-- theme
-	color_scheme = "Firewatch",
-
-	-- Tabs bar
-	hide_tab_bar_if_only_one_tab = true,
-
-	-- Window
-	window_decorations = "NONE",
-	window_padding = { left = 5, right = 5, top = 5, bottom = 5 },
-	initial_cols = 165,
-	initial_rows = 50,
-}
-=======
+config.max_fps = 120
 --- Appearance ---
 config.font_size = 13.0
 config.color_scheme = "Firewatch"
@@ -34,4 +13,3 @@ config.window_decorations = "NONE"
 config.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
 
 return config
->>>>>>> 8847c20e1a964150ff879daec0307d59d0e9af3c
